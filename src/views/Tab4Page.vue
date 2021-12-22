93def6cb3746ec6541d7da738a3e32adc90bc4fd
Shinerutt <ballbaba1999@gmail.com>
2021-12-22 21:57:17 +0700

    install ionic finish

--- src/views/Tab4Page.vue
+++ src/views/Tab4Page.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Tab 4</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab 4</ion-title>
         </ion-toolbar>
       </ion-header>
-     <H1>Hello world</H1>
-      <ExploreContainer name="Tab 1 page" />
+      
+      <ExploreContainer name="Tab 4 page" />
     </ion-content>
   </ion-page>
 </template>
